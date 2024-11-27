cc03d8719050806675042fb863882da567d64a05
Vosnie <vosnie@gmail.com>
2024-11-27 10:23:27 +0100

    website
    The Real WoodSmith website setup

---
+++ code/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home')
+
+@app.route('/about')
+
+def about():
+    return render_template('./about')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('./portfolio.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('./contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
